9d79dac8d2ef0af9fbf6954ba58d6bd1dcba963d
ZohnNasse <97154206+ZohnNasse@users.noreply.github.com>
2023-05-31 17:33:17 +0900

    프로젝트 메뉴 추가

--- page3/assets/js/script.js
+++ page3/assets/js/script.js
@@ -1,6 +1,10 @@
 // 홈 페이지로 이동하는 함수
 function goToHomePage() {
-  window.location.href = 'index.html'; // 홈 페이지로 이동
+  const currentUrl = window.location.href;
+  const homeUrl = 'index.html';
+  if (currentUrl !== homeUrl) {
+    window.location.href = homeUrl;
+  }
 }
 
 // menu-icons의 모든 이미지 요소를 선택합니다.
@@ -9,7 +13,7 @@ const menuIcons = document.querySelectorAll('.menu-icons img');
 // 각 이미지 요소에 마우스 이벤트 리스너를 등록합니다.
 menuIcons.forEach((icon) => {
   // 마우스 진입 시 이벤트 핸들러
-  icon.addEventListener('mouseenter', () => {
+  icon.addEventListener('mouseenter', (event) => {
     const altText = icon.getAttribute('alt'); // 이미지의 alt 속성값을 가져옵니다.
     const tooltip = document.createElement('div'); // 새로운 div 요소를 생성합니다.
     tooltip.classList.add('tooltip'); // tooltip 클래스를 추가합니다.
@@ -38,6 +42,7 @@ menuIcons.forEach((icon) => {
     icon.addEventListener('click', goToHomePage); // 새로운 이벤트 리스너 등록
   }
 });
+
 // project 메뉴 클릭 시 모니터 컨테이너 내용 변경
 const projectMenu = document.getElementById('project-menu');
 const monitorContainer = document.querySelector('.monitor .content');
@@ -53,14 +58,14 @@ projectMenu.addEventListener('click', () => {
   monitorContainer.appendChild(projectContent);
 });
 
-window.onload = function () {
+window.onload = function() {
   const titleElement = document.getElementById('title');
   const descriptionElement = document.getElementById('description');
   const text =
     '안녕하세요. 저는 비전공으로 AI를 배우고 있는 Zohn이라고 합니다. 이곳은 ChatGPT와 함께 만드는 홈페이지입니다. 앞으로 이 페이지를 통해 쿼티 킹덤이라는 타자 게임을 런칭 할 예정입니다. 많은 관심 부탁 드립니다.';
   let index = 0;
 
-  function showText() {
+  function showText(event) {
     if (index < text.length) {
       if (text[index] === '.') {
         titleElement.innerHTML += text[index] + '<br>'; // 마침표일 경우 마침표와 줄바꿈 추가
@@ -69,10 +74,10 @@ window.onload = function () {
       }
       if (window.innerWidth <= 767) {
         titleElement.style.fontSize = '10px'; // 모바일 환경에서 글자 크기를 작게 설정
-        setTimeout(showText, 120); // 모바일 환경에서 글자가 더 작으므로 간격을 늘림 (150ms)
+        setTimeout(showText, 120, event); // 모바일 환경에서 글자가 더 작으므로 간격을 늘림 (150ms)
       } else {
         titleElement.style.fontSize = '16px'; // 기본 글자 크기
-        setTimeout(showText, 100); // 한 글자씩 표시하는 간격 (100ms)
+        setTimeout(showText, 100, event); // 한 글자씩 표시하는 간격 (100ms)
       }
       index++;
     }
@@ -91,39 +96,15 @@ window.onload = function () {
 
   adjustMenuIconSize(); // 초기 설정
 
-  window.addEventListener('resize', adjustMenuIconSize); // 화면 크기 변경 시 메뉴 아이콘 크기 조정
-  window.addEventListener('resize', function() {
-    const menuIcons = document.querySelectorAll('.menu-icons img');
-  
-    // 현재 브라우저 너비를 확인하여 모바일 환경인지 판단
+  window.addEventListener('resize', function(event) {
+    adjustMenuIconSize();
+
     const isMobile = window.innerWidth <= 767;
-  
-    // 모바일 환경인 경우 아이콘 크기를 작게 설정, 아닌 경우는 초기 크기로 설정
     const iconSize = isMobile ? '24px' : '32px';
-  
-    // 각 아이콘의 크기를 설정
+
     menuIcons.forEach((icon) => {
       icon.style.width = iconSize;
       icon.style.height = iconSize;
     });
   });
-  
-  // 초기 로드 시에도 크기를 설정하기 위해 resize 이벤트를 발생시킴
-  window.dispatchEvent(new Event('resize'));
 };
-
-// project 메뉴 클릭 시 모니터 컨테이너 내용 변경
-const projectMenu = document.getElementById('project-menu');
-const monitorContainer = document.querySelector('.monitor .content');
-
-projectMenu.addEventListener('click', () => {
-  // 모니터 컨테이너 내용 비우기
-  monitorContainer.innerHTML = '';
-
-  // 새로운 내용 추가
-  const projectContent = document.createElement('div');
-  projectContent.innerHTML = '<h2>프로젝트 내용</h2><p>프로젝트에 대한 설명이 여기에 작성됩니다.</p>';
-
-  monitorContainer.appendChild(projectContent);
-});
-
