005603651558d421eb6b0d28baecc7fe62449a4a
ZohnNasse <k12j34@gmail.com>
2023-06-02 18:30:22 +0900

    메뉴 스크립트 수정

--- page3/assets/js/menu.js
+++ page3/assets/js/menu.js
@@ -4,19 +4,19 @@ const menuIcons = document.querySelectorAll('.menu-icons img')
 // 각 이미지 요소에 마우스 이벤트 리스너를 등록합니다.
 menuIcons.forEach((icon) => {
   // 마우스 진입 시 이벤트 핸들러
-  icon.addEventListener('mouseenter', () => {
-    const altText = icon.getAttribute('alt') // 이미지의 alt 속성값을 가져옵니다.
-    const tooltip = document.createElement('div') // 새로운 div 요소를 생성합니다.
-    tooltip.classList.add('tooltip') // tooltip 클래스를 추가합니다.
-    tooltip.textContent = altText // tooltip의 텍스트를 alt 속성값으로 설정합니다.
+  icon.addEventListener('mouseenter', (event) => {
+    // 이벤트 객체를 인자로 받아 사용합니다.
+    const altText = icon.getAttribute('alt')
+    const tooltip = document.createElement('div')
+    tooltip.classList.add('tooltip')
+    tooltip.textContent = altText
 
-    // 마우스 커서 위치에 툴팁을 표시하기 위해 좌표를 계산합니다.
     const mouseX = event.clientX
     const mouseY = event.clientY
-    tooltip.style.left = `${mouseX + 10}px` // 툴팁을 마우스 커서 오른쪽으로 10px 이동시킵니다.
-    tooltip.style.top = `${mouseY}px` // 툴팁을 마우스 커서와 동일한 높이로 설정합니다.
+    tooltip.style.left = `${mouseX + 10}px`
+    tooltip.style.top = `${mouseY}px`
 
-    document.body.appendChild(tooltip) // body 요소에 tooltip을 추가합니다.
+    document.body.appendChild(tooltip)
   })
 
   // 마우스 이탈 시 이벤트 핸들러
@@ -27,3 +27,34 @@ menuIcons.forEach((icon) => {
     }
   })
 })
+
+// 각 이미지 요소에 클릭 이벤트 리스너를 등록합니다.
+menuIcons.forEach((icon) => {
+  icon.addEventListener('click', () => {
+    const altText = icon.getAttribute('alt') // 이미지의 alt 속성값을 가져옵니다.
+
+    // 모니터 컨테이너의 내용을 비웁니다.
+    const monitorContainer = document.querySelector('.monitor .content')
+    monitorContainer.innerHTML = ''
+
+    // alt 속성값에 따라 해당하는 스크립트를 로드합니다.
+    switch (altText) {
+      case 'Home':
+        loadScript('home.js')
+        break
+      case 'Project':
+        loadScript('project.js')
+        break
+      case 'Contact':
+        loadScript('contact.js')
+        break
+    }
+  })
+})
+
+// 스크립트를 동적으로 로드하는 함수입니다.
+function loadScript(filename) {
+  const script = document.createElement('script')
+  script.src = filename
+  document.body.appendChild(script)
+}
