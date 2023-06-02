5e37b60133da106c84a96056fd0e252e7562929c
ZohnNasse <k12j34@gmail.com>
2023-06-02 23:31:42 +0900

    style.css script.js 파일 수정

--- page3/assets/js/script.js
+++ page3/assets/js/script.js
@@ -1,17 +1,4 @@
-// 홈 페이지로 이동하는 함수
-function goToHomePage() {
-  const currentUrl = window.location.href
-  const homeUrl = 'index.html'
-  if (currentUrl !== homeUrl) {
-    window.location.href = homeUrl
-  }
-}
-
-adjustMenuIconSize() // 초기 설정
-
 window.addEventListener('resize', function (event) {
-  adjustMenuIconSize()
-
   const isMobile = window.innerWidth <= 767
   const iconSize = isMobile ? '24px' : '32px'
 
