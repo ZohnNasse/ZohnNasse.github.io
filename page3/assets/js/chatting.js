60c8565dfa130c4a9d72213728bcac6c9240a80f
ZohnNasse <k12j34@gmail.com>
2023-06-05 16:21:29 +0900

    프로젝트내 스크립트 파일 생성

---
+++ page3/assets/js/chatting.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1 id="myHeading">준비중</h1>
+
+<script>
+document.getElementById("myHeading").innerHTML = "Hello World!";
+</script>
+
+</body>
+</html>
--- page3/assets/js/project.js
+++ page3/assets/js/project.js
@@ -1,13 +1,11 @@
 ;(function () {
   const monitorContainer = document.querySelector('.monitor .content')
   monitorContainer.innerHTML = '' // 모니터 컨테이너의 내용을 비웁니다.
-
   const projectIcons = ['weather.png', 'chatting.png', 'GenImg.png'] // 아이콘 파일 이름 배열
   const projectScripts = ['weather.js', 'chatting.js', 'GenImg.js'] // 각 아이콘에 연결될 스크립트 파일 이름 배열
-
   projectIcons.forEach((icon, index) => {
     const projectIcon = document.createElement('img')
-    projectIcon.src = `./assets/images/${icon}` // 아이콘 이미지 경로 설정
+    projectIcon.src = `../assets/images/${icon}` // 아이콘 이미지 경로 설정
     projectIcon.style.width = '20%' // 아이콘 너비 설정
     projectIcon.style.height = '20%' // 아이콘 높이 설정
     projectIcon.addEventListener('click', () => {
